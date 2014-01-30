e9bbeb8b372214525d2adecf33f3dc724fb1c6ac
chrihill627 <yorkshiretable@gmail.com>
2014-01-30 13:37:36 +0000

    layout problem sorted

--- server.rb
+++ server.rb
@@ -1,9 +1,17 @@
-# class WelcomeGreeting
-
-#   def initilize
-#     @time = (Time.now.strftime("%I").to_i)
-#   end
 require 'sinatra'
+require 'rest-client'
+require 'twilio-ruby'
+
+
+  def text(phonenum)
+    account_sid = 'ACecb4b1f21565ecc93c2a02d93fce811d'
+    auth_token = '8820720da1cae123afda99970ed76bcd'
+    @client = Twilio::REST::Client.new account_sid, auth_token    
+    sms = @client.account.sms.messages.create(:body => "Jenny please?! I love you <3",
+        :to => phonenum,
+        :from => "+441301272034")
+    puts sms.from
+  end
 
 
   def greeting(time = Time.now)
@@ -17,20 +25,37 @@ require 'sinatra'
     p greeting
     greeting
   end
-  #code will display good morning at 12:30
+
+  def twitterscrape
+    
+  end
+
+  def send_simple_message(mailaddress)
+    RestClient.post "https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0"\
+    "@api.mailgun.net/v2/samples.mailgun.org/messages",
+    :from => "Chris Hill <excited@samples.mailgun.org>",
+    :to => mailaddress,
+    :subject => "Hello",
+    :text => "Testing some Mailgun awesomeness!"
+  end
 
 
-#end
+  post '/send_simple_message' do
+    send_simple_message(params[:mailaddress])
+    redirect '/#contact'
+  end
 
+  post '/send_text' do
+    text(params[:phonenum])
+    redirect '/#contact'
+  end
 
-#put this in your server rb
-get "/" do 
+  get "/" do 
+   @greeting = greeting
+   erb :index
+  end
 
- @greeting = greeting
-
-  #this goes to your views/index.erb
-  erb :index
-end
-
-#in your erb
+  get "/blog" do
+    erb :blog
+  end
 
